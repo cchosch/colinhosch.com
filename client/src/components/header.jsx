17ae08425c8934270c5425b0b945a8840730180f
cchosch <colinhoscheit@gmail.com>
2022-03-30 00:35:10 -0400

    front page coming along quite a bit

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -2,8 +2,22 @@ import "./header.css";
 import NavBar from "./navbar.jsx";
 
 function Header(){
+    var today = new Date();
+    var month = today.getMonth();
+    var age = today.getFullYear()-2006;
+    if(month === 5){
+        if(today.getDate() < 26)
+            age -= 1
+    }else if(month < 5){
+        age -= 1
+    }
     return (<div className="header">
         <NavBar/>
+        <div className="missionStatement">I'm a Self-Taught Developer</div>
+        <div className="info">
+            <div className= "infoCell leftInfo"><i>Who?</i></div>
+            <div className="infoCell">My name is Colin Hoscheit and I'm a {age} year old self-taught software developer in the Washington DC area.</div>
+        </div>
     </div>);
 }
 
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -5,14 +5,13 @@ function NavBar(){
     //               name: path
     const links = [{"Portfolio": "/"}, {"About": "/"}];
     return (<div className="navBar">
-        <div className="navSection">
+        <div className="navSection left">
         {links.map((v) => {
             return (<a className="navLink" href={v[Object.keys(v)[0]]}>{Object.keys(v)[0]}</a>)
         })}
         </div>
         <div className="navSection">
-            <p className="headName">oo</p>
-            
+            <a className="headName" href="/">Colin Hoscheit</a>
         </div>
     </div>)
 
